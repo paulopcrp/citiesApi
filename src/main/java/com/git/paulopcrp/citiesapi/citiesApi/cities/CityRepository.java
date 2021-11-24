179e690d9a89d6c350e54877f7f7f94ad0671bed
“paulopcrp” <paulo.pcrp@gmail.com>
2021-11-24 01:45:10 -0800

    API de listagem completa e funcional

---
+++ src/main/java/com/git/paulopcrp/citiesapi/citiesApi/cities/CityRepository.java
@@ -0,0 +1,6 @@
+package com.git.paulopcrp.citiesapi.citiesApi.cities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
---
+++ src/main/java/com/git/paulopcrp/citiesapi/citiesApi/staties/StateRepository.java
@@ -0,0 +1,8 @@
+package com.git.paulopcrp.citiesapi.citiesApi.staties;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+
+
+}
